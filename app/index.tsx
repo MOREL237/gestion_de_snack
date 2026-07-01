50ea14f608e160b11abb7c15e40539aafe953edd
MOREL237 <moreltiamo@gmail.com>
2026-07-01 02:40:21 -0700

    initial project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-xl font-bold text-white">
+        Setup pour executer une application mobile avec Expo-Router et NativeWind 
+      </Text>
+    </View>
+  );
+}
